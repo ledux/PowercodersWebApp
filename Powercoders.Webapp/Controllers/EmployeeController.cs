d1bfe26fb0177abf8d33c4c05d0412aa12a423a8
Lukas Tanner <lueq@lueqs-artix.local>
2022-11-06 12:17:54 +0100

    implement the create action

--- Powercoders.Webapp/Controllers/EmployeeController.cs
+++ Powercoders.Webapp/Controllers/EmployeeController.cs
@@ -5,23 +5,61 @@ namespace Powercoders.Webapp.Controllers;
 
 public class EmployeeController : Controller
 {
+    private static readonly IList<Employee> EmployeeRepository = new List<Employee>
+    {
+        new(1, "Lukas", "Tanner", "lukas.tanner@example.com"),
+        new(2, "Eric", "Idle", "eric.idle@example.com")
+    };
+
+    private static readonly IList<string> EmailRepository = new List<string>
+    {
+        "lukas.tanner@example.com",
+        "eric.idle@example.com"
+    };
+    
     // GET
     public IActionResult Index()
     {
-        return View(new Employee[]
+        return View(EmployeeRepository);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    
+    [HttpPost]
+    public IActionResult Create(Employee employee)
+    {
+        var employeeEmail = $"{employee.Firstname.ToLower()}.{employee.Lastname.ToLower()}@example.com";
+        if (EmailRepository.Contains(employeeEmail))
         {
-            new()
-            {
-                Firstname = "Lukas",
-                Lastname = "Tanner",
-                Email = "lukas.tanner@example.com"
-            },
-            new()
-            {
-                Firstname = "Lukas",
-                Lastname = "Tanner",
-                Email = "lukas.tanner@example.com"
-            }
-        });
+            return View("Error", new ErrorViewModel());
+        }
+
+        var nextId = EmployeeRepository.OrderBy(e => e.Id).Last().Id + 1;
+        var createdEmployee = employee with{ Email = employeeEmail, Id = nextId };
+
+        EmailRepository.Add(employeeEmail);
+        EmployeeRepository.Add(createdEmployee);
+        
+        return RedirectToAction("Index");
+    }
+
+
+    public IActionResult Edit(string employeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Details(string employeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Delete(string employeeId)
+    {
+        throw new NotImplementedException();
     }
 }
--- Powercoders.Webapp/Models/Employee.cs
+++ Powercoders.Webapp/Models/Employee.cs
@@ -1,8 +1,9 @@
 namespace Powercoders.Webapp.Models;
 
-public class Employee
+public record Employee(uint? Id, string Firstname, string Lastname, string Email)
 {
-    public string Firstname { get; set; }
-    public string Lastname { get; set; }
-    public string Email { get; set; }
+    public uint? Id { get; set; } = Id;
+    public string Firstname { get; set; } = Firstname;
+    public string Lastname { get; set; } = Lastname;
+    public string Email { get; set; } = Email;
 }
